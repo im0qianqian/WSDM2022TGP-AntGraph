b2e8cbf4476d24b43626055a9a4ec853860630ec
im0qianqian <qian1335661317@qq.com>
2021-11-30 20:20:57 +0800

    format ⚒

--- baseline/base_pipeline.py
+++ baseline/base_pipeline.py
@@ -15,6 +15,7 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import roc_auc_score
 
+
 def get_args():
     ### Argument and global variables
     parser = argparse.ArgumentParser('Base')
@@ -32,6 +33,7 @@ def get_args():
         sys.exit(0)
     return args
 
+
 class HeteroConv(nn.Module):
     def __init__(self, etypes, n_layers, in_feats, hid_feats, activation, dropout=0.2):
         super(HeteroConv, self).__init__()
@@ -43,30 +45,31 @@ class HeteroConv(nn.Module):
         self.dropout = nn.Dropout(p=dropout, inplace=True)
         self.hconv_layers = nn.ModuleList()
         self.norms = nn.ModuleList()
-        for i in range(n_layers+1):
-            self.norms.append(nn.BatchNorm1d(hid_feats)) 
+        for i in range(n_layers + 1):
+            self.norms.append(nn.BatchNorm1d(hid_feats))
 
-        # input layer
-        self.hconv_layers.append(self.build_hconv(in_feats,hid_feats,activation=self.act))
+            # input layer
+        self.hconv_layers.append(self.build_hconv(in_feats, hid_feats, activation=self.act))
         # hidden layers
         for i in range(n_layers - 1):
-            self.hconv_layers.append(self.build_hconv(hid_feats,hid_feats,activation=self.act))   
-        # output layer
-        self.hconv_layers.append(self.build_hconv(hid_feats,hid_feats)) # activation None        
+            self.hconv_layers.append(self.build_hconv(hid_feats, hid_feats, activation=self.act))
+            # output layer
+        self.hconv_layers.append(self.build_hconv(hid_feats, hid_feats))  # activation None
 
-        self.fc1 = nn.Linear(hid_feats*2+10, hid_feats)
+        self.fc1 = nn.Linear(hid_feats * 2 + 10, hid_feats)
         self.fc2 = nn.Linear(hid_feats, 1)
-    
-    def build_hconv(self,in_feats,out_feats,activation=None):
+
+    def build_hconv(self, in_feats, out_feats, activation=None):
         GNN_dict = {}
         for event_type in self.etypes:
-            GNN_dict[event_type] = dglnn.SAGEConv(in_feats=in_feats,out_feats=out_feats,aggregator_type='mean',activation=activation) 
+            GNN_dict[event_type] = dglnn.SAGEConv(in_feats=in_feats, out_feats=out_feats, aggregator_type='mean',
+                                                  activation=activation)
         return dglnn.HeteroGraphConv(GNN_dict, aggregate='sum')
 
     def forward(self, g, feat_key='feat'):
         h = g.ndata[feat_key]
-        if not isinstance(h,dict):
-            h = {'Node':g.ndata[feat_key]}
+        if not isinstance(h, dict):
+            h = {'Node': g.ndata[feat_key]}
         for i, layer in enumerate(self.hconv_layers):
             h = layer(g, h)
             for key in h.keys():
@@ -75,22 +78,23 @@ class HeteroConv(nn.Module):
 
     def emb_concat(self, g, etype):
         def cat(edges):
-            return {'emb_cat': torch.cat([edges.src['emb'], edges.dst['emb']],1)}
+            return {'emb_cat': torch.cat([edges.src['emb'], edges.dst['emb']], 1)}
+
         with g.local_scope():
             g.apply_edges(cat, etype=etype)
             emb_cat = g.edges[etype].data['emb_cat']
         return emb_cat
 
-    def time_encoding(self, x, bits=10): 
+    def time_encoding(self, x, bits=10):
         '''
         This function is designed to encode a unix timestamp to a 10-dim vector. 
         And it is only one of the many options to encode timestamps.
         Users can also define other time encoding methods such as Neural Network based ones.
         '''
-        inp = x.repeat(10,1).transpose(0,1)
-        div = torch.cat([torch.ones((x.shape[0],1))*10**(bits-1-i) for i in range(bits)],1)
-        return (((inp/div).int()%10)*0.1).float()
-        
+        inp = x.repeat(10, 1).transpose(0, 1)
+        div = torch.cat([torch.ones((x.shape[0], 1)) * 10 ** (bits - 1 - i) for i in range(bits)], 1)
+        return (((inp / div).int() % 10) * 0.1).float()
+
     def time_predict(self, node_emb_cat, time_emb):
         h = torch.cat([node_emb_cat, time_emb], 1)
         h = self.dropout(h)
@@ -98,10 +102,11 @@ class HeteroConv(nn.Module):
         h = self.act(h)
         h = self.fc2(h)
         return h
-    
+
+
 def train(args, g):
     if args.dataset == 'B':
-        dim_nfeat = args.emb_dim*2 
+        dim_nfeat = args.emb_dim * 2
         for ntype in g.ntypes:
             g.nodes[ntype].data['feat'] = torch.randn((g.number_of_nodes(ntype), dim_nfeat)) * 0.05
     else:
@@ -110,9 +115,9 @@ def train(args, g):
     model = HeteroConv(g.etypes, args.n_layers, dim_nfeat, args.emb_dim, F.relu)
 
     optimizer = torch.optim.AdamW(model.parameters(),
-                                 lr=args.lr,
-                                 weight_decay=args.weight_decay)
-    loss_fcn = nn.BCEWithLogitsLoss()  
+                                  lr=args.lr,
+                                  weight_decay=args.weight_decay)
+    loss_fcn = nn.BCEWithLogitsLoss()
     loss_values = []
 
     for i in range(args.epochs):
@@ -131,18 +136,18 @@ def train(args, g):
             idx = torch.randperm(ts.shape[0])
             ts_shuffle = ts[idx]
             neg_label = torch.zeros_like(ts)
-            neg_label[ts_shuffle>=ts] = 1
+            neg_label[ts_shuffle >= ts] = 1
 
-            time_emb = model.time_encoding(ts) 
-            time_emb_shuffle = model.time_encoding(ts_shuffle) 
+            time_emb = model.time_encoding(ts)
+            time_emb_shuffle = model.time_encoding(ts_shuffle)
 
             pos_exist_prob = model.time_predict(emb_cat, time_emb).squeeze()
             neg_exist_prob = model.time_predict(emb_cat, time_emb_shuffle).squeeze()
 
-            probs = torch.cat([pos_exist_prob,neg_exist_prob],0)
-            label = torch.cat([torch.ones_like(ts),neg_label],0).float()
-            loss += loss_fcn(probs, label)/len(g.etypes)
-                
+            probs = torch.cat([pos_exist_prob, neg_exist_prob], 0)
+            label = torch.cat([torch.ones_like(ts), neg_label], 0).float()
+            loss += loss_fcn(probs, label) / len(g.etypes)
+
         loss_values.append(loss.item())
         loss.backward()
         optimizer.step()
@@ -152,8 +157,8 @@ def train(args, g):
         # test every epoch
         test(args, g, model)
     return g, model
-    
-    
+
+
 def preprocess(args, directed_g):
     # this function is used to add reverse edges for model computing
     if args.dataset == 'A':
@@ -161,10 +166,10 @@ def preprocess(args, directed_g):
     if args.dataset == 'B':
         graph_dict = {}
         for (src_type, event_type, dst_type) in directed_g.canonical_etypes:
-            graph_dict[(src_type, event_type, dst_type)] = directed_g.edges(etype = (src_type, event_type, dst_type))
-            src_nodes_reversed = directed_g.edges(etype = (src_type, event_type, dst_type))[1]
-            dst_nodes_reversed = directed_g.edges(etype = (src_type, event_type, dst_type))[0]
-            graph_dict[(dst_type, event_type+'_reversed', src_type)] = (src_nodes_reversed,dst_nodes_reversed)
+            graph_dict[(src_type, event_type, dst_type)] = directed_g.edges(etype=(src_type, event_type, dst_type))
+            src_nodes_reversed = directed_g.edges(etype=(src_type, event_type, dst_type))[1]
+            dst_nodes_reversed = directed_g.edges(etype=(src_type, event_type, dst_type))[0]
+            graph_dict[(dst_type, event_type + '_reversed', src_type)] = (src_nodes_reversed, dst_nodes_reversed)
         g = dgl.heterograph(graph_dict)
         for etype in g.etypes:
             g.edges[etype].data['ts'] = directed_g.edges[etype.split('_')[0]].data['ts']
@@ -172,36 +177,39 @@ def preprocess(args, directed_g):
                 g.edges[etype].data['feat'] = directed_g.edges[etype.split('_')[0]].data['feat']
     return g
 
+
 @torch.no_grad()
 def test(args, g, model):
     model.eval()
-    test_csv = pd.read_csv(f'test_csvs/input_{args.dataset}_initial.csv', names=['src', 'dst', 'type', 'start_at', 'end_at', 'exist'])
+    test_csv = pd.read_csv(f'test_csvs/input_{args.dataset}_initial.csv',
+                           names=['src', 'dst', 'type', 'start_at', 'end_at', 'exist'])
     label = test_csv.exist.values
     src = test_csv.src.values
     dst = test_csv.dst.values
     start_at = torch.tensor(test_csv.start_at.values)
     end_at = torch.tensor(test_csv.end_at.values)
     if args.dataset == 'A':
-        emb_cats =  torch.cat([g.ndata['emb'][src],g.ndata['emb'][dst]], 1)
+        emb_cats = torch.cat([g.ndata['emb'][src], g.ndata['emb'][dst]], 1)
     if args.dataset == 'B':
-        emb_cats =  torch.cat([g.ndata['emb']['User'][src],g.ndata['emb']['Item'][dst]], 1)
+        emb_cats = torch.cat([g.ndata['emb']['User'][src], g.ndata['emb']['Item'][dst]], 1)
 
     start_time_emb = model.time_encoding(start_at)
     end_time_emb = model.time_encoding(end_at)
     start_prob = model.time_predict(emb_cats, start_time_emb).squeeze()
     end_prob = model.time_predict(emb_cats, end_time_emb).squeeze()
     exist_prob = end_prob - start_prob
-    
-    AUC = roc_auc_score(label,exist_prob)
-    print(f'AUC is {round(AUC,5)}')
+
+    AUC = roc_auc_score(label, exist_prob)
+    print(f'AUC is {round(AUC, 5)}')
+
 
 if __name__ == "__main__":
     args = get_args()
     if args.dataset == 'B':
-        g= dgl.load_graphs(f'DGLgraphs/Dataset_{args.dataset}.bin')[0][0]
+        g = dgl.load_graphs(f'DGLgraphs/Dataset_{args.dataset}.bin')[0][0]
     elif args.dataset == 'A':
         g, etype_feat = dgl.load_graphs(f'DGLgraphs/Dataset_{args.dataset}.bin')
         g = g[0]
     g = preprocess(args, g)
-    g, model = train(args,g)
+    g, model = train(args, g)
     test(args, g, model)
--- baseline/csv2DGLgraph.py
+++ baseline/csv2DGLgraph.py
@@ -6,44 +6,44 @@ import torch
 import dgl
 import numpy as np
 
+
 def csv2graph(args):
     if args.dataset == 'A':
         src_type = 'Node'
         dst_type = 'Node'
-    elif args.dataset == 'B':    
+    elif args.dataset == 'B':
         src_type = 'User'
         dst_type = 'Item'
     else:
         print(' Input parameters error')
 
-    edge_csv = pd.read_csv(f'train_csvs/edges_train_{args.dataset}.csv',header=None)
-    
+    edge_csv = pd.read_csv(f'train_csvs/edges_train_{args.dataset}.csv', header=None)
+
     heterogenous_group = edge_csv.groupby(2)
     graph_dict = {}
     ts_dict = {}
 
     for event_type, records in heterogenous_group:
         event_type = str(event_type)
-        graph_dict[(src_type, event_type, dst_type)] = (records[0].to_numpy(),records[1].to_numpy())
+        graph_dict[(src_type, event_type, dst_type)] = (records[0].to_numpy(), records[1].to_numpy())
         ts_dict[(src_type, event_type, dst_type)] = (torch.FloatTensor(records[3].to_numpy()))
     g = dgl.heterograph(graph_dict)
-    
-    g.edata['ts'] = ts_dict
 
+    g.edata['ts'] = ts_dict
 
     if args.dataset == 'A':
         # Assign Node feature in to graph
-        node_feat_csv = pd.read_csv('train_csvs/node_features.csv',header=None)
-        node_feat = node_feat_csv.values[:,1:]
-        node_idx = node_feat_csv.values[:,0]
+        node_feat_csv = pd.read_csv('train_csvs/node_features.csv', header=None)
+        node_feat = node_feat_csv.values[:, 1:]
+        node_idx = node_feat_csv.values[:, 0]
         g.nodes[src_type].data['feat'] = torch.zeros((g.number_of_nodes(src_type), 8))
         g.nodes[src_type].data['feat'][node_idx] = torch.FloatTensor(node_feat)
-        
+
         # Assign Edge Type Feature as the graph`s label, which can be saved along with dgl.heterograph
-        etype_feat_csv = pd.read_csv('train_csvs/edge_type_features.csv',header=None)
-        etype_feat_tensor = torch.FloatTensor(etype_feat_csv.values[:,1:])
+        etype_feat_csv = pd.read_csv('train_csvs/edge_type_features.csv', header=None)
+        etype_feat_tensor = torch.FloatTensor(etype_feat_csv.values[:, 1:])
         etype_feat = {}
-        for i,etype in enumerate(g.etypes):
+        for i, etype in enumerate(g.etypes):
             etype_feat[etype] = etype_feat_tensor[i]
 
         dgl.save_graphs(f"./DGLgraphs/Dataset_{args.dataset}.bin", g, etype_feat)
@@ -56,12 +56,10 @@ def csv2graph(args):
             etype = (src_type, event_type, dst_type)
             if len(str(records[4].iloc[0])) > 3:
                 g.edges[etype].data['feat'] = (extract_edge_feature(records[4]))
-        dgl.save_graphs(f"./DGLgraphs/Dataset_{args.dataset}.bin", g)   
+        dgl.save_graphs(f"./DGLgraphs/Dataset_{args.dataset}.bin", g)
 
-    
 
 def extract_edge_feature(records):
-
     # if you face with OOM issue, using looping method istead of mapping method
 
     # parallelism mapping method
@@ -73,20 +71,22 @@ def extract_edge_feature(records):
     # for record in records:
     #     feat_l.append(record.strip().split(','))
     # feat = torch.FloatTensor(np.array(feat_l).astype('float32'))  
-      
+
     return feat
+
 
 def get_args():
     ### Argument and global variables
     parser = argparse.ArgumentParser('csv2DGLgraph')
-    parser.add_argument('--dataset', type=str, choices=["A", "B"], default = 'A', help='Dataset name')
+    parser.add_argument('--dataset', type=str, choices=["A", "B"], default='A', help='Dataset name')
     try:
         args = parser.parse_args()
     except:
         parser.print_help()
         sys.exit(0)
-    
+
     return args
+
 
 if __name__ == "__main__":
     args = get_args()
@@ -101,4 +101,3 @@ if __name__ == "__main__":
         os.system('wget -P test_csvs https://data.dgl.ai/dataset/WSDMCup2022/input_B_initial.csv.gz')
         os.system('gzip -d test_csvs/*.gz')
     csv2graph(args)
-    
