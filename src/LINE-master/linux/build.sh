d056f311c1031ab584cb170d9b9f85a4e7640c95
im0qianqian <qian1335661317@qq.com>
2022-01-23 21:35:02 +0800

    upload the LINE mothed

---
+++ src/LINE-master/linux/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+g++ -lm -pthread -Ofast -march=native -Wall -funroll-loops -ffast-math -Wno-unused-result -std=c++11 line.cpp -o line -lgsl -lm -lgslcblas
+g++ -lm -pthread -Ofast -march=native -Wall -funroll-loops -ffast-math -Wno-unused-result reconstruct.cpp -o reconstruct
+g++ -lm -pthread -Ofast -march=native -Wall -funroll-loops -ffast-math -Wno-unused-result normalize.cpp -o normalize
+g++ -lm -pthread -Ofast -march=native -Wall -funroll-loops -ffast-math -Wno-unused-result concatenate.cpp -o concatenate
